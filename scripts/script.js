f0914870d4396d70e5f9462c4eaf6f649666dc04
dungeoncompany <dungeoncompany@mail.ru>
2023-04-26 21:38:45 +0300

    mega update

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 console.log("Start...");
+
+var buttonMenu = document.getElementById("button-menu");
+var menuMobile = document.getElementById("menu-mobile");
+var menuIcon = document.querySelector(".menu-icon");
+
+buttonMenu.addEventListener("click", function () {
+  if (menuMobile.style.display === "block") {
+    menuMobile.style.display = "none";
+    menuIcon.classList.remove("rotated");
+  } else {
+    menuMobile.style.display = "block";
+    menuIcon.classList.add("rotated");
+  }
+});
